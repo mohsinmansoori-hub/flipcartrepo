04dd5caea1cc29c603acb86b3c0630f1c0d78c06
Mohsin <mohsinmansoori0594@gmail.com>
2020-12-21 20:22:22 +0530

    signup page modified

--- src/com/register/testscripts/SignupPage.java
+++ src/com/register/testscripts/SignupPage.java
@@ -2,6 +2,11 @@ package com.register.testscripts;
 
 public class SignupPage {
 	public static void main(String args[]) {
+		
+System.out.println("page actions");
+
+System.out.println("methods");
+
 		System.out.println("sign up page");
 	}
 
